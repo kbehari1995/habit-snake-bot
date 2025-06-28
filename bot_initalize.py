bd9d37476d4f63a7b38b221d46a7ada4cd2255fe
Kunj <kbehari@gmail.com>
2025-06-28 20:34:07 +0530

    Finalize setup: add .env support, README, and requirements

--- bot_initalize.py
+++ bot_initalize.py
@@ -1,10 +1,17 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SHEET_ID = os.getenv("SHEET_ID")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I’m your first bot 🐣")
 
-app = ApplicationBuilder().token("8159556087:AAEXRFmt3jtJ4KFkTUXGym-XBXScEAN-2Ts").build()
+app = ApplicationBuilder().token(SHEET_ID).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
--- gsheets_test_entry.py
+++ gsheets_test_entry.py
@@ -1,10 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SHEET_ID = os.getenv("SHEET_ID")
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 
-sheet = client.open_by_key("1pShshf-q3D329ObcSOIRHiLxpO2VPKwEHzjnUj0mxy8").sheet1
-sheet.append_row(["2025-06-28", "kunj", "Meditation", "✅"])
+sheet = client.open_by_key(SHEET_ID).sheet1
+sheet.append_row(["2025-07-28", "kunj", "Meditation", "✅"])
+
 
