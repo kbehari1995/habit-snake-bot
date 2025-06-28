826e86675f1ca480e933c1643d84c18661ac6bfe
Kunj <kbehari@gmail.com>
2025-06-28 21:08:00 +0530

    Main py readied for testing

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # main.py
 from telegram.ext import Updater
-from handlers.sethabits import set_habits_handler
+#from handlers.sethabits import set_habits_handler
+import os
+from dotenv import load_dotenv
 
-from config import BOT_TOKEN
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SHEET_ID = os.getenv("SHEET_ID")
+
 
 def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     # Register handlers
-    dp.add_handler(set_habits_handler)
+    #dp.add_handler(set_habits_handler)
 
     print("✅ Bot running...")
     updater.start_polling()
